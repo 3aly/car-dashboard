e45e3866a3b7e7aff1e801e53ec046b586224db1
3aly <thebigali1998@gmail.com>
2022-10-18 01:22:08 +0200

    messages functional (no styling)

---
+++ src/pages/Messages/data.js
@@ -0,0 +1,260 @@
+import user1 from "../Icons/user1.png";
+import user2 from "../Icons/user2.png";
+
+const chatData = [
+  {
+    id: 1,
+    imageUrl: user1,
+    contactName: "Killan James",
+    isPinned: true,
+    messages: [
+      {
+        id: 1,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:30 PM",
+        type: "reciever",
+      },
+      {
+        id: 2,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:40 PM",
+        type: "sender",
+      },
+      {
+        id: 3,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice 😍",
+        timeStamp: "4:50 PM",
+        type: "reciever",
+      },
+      {
+        id: 4,
+        content:
+          " i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:00 PM",
+        type: "reciever",
+      },
+      {
+        id: 5,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:10 PM",
+        type: "sender",
+      },
+    ],
+  },
+  {
+    id: 2,
+    imageUrl: user2,
+    contactName: "Desian Tam",
+    isPinned: true,
+    messages: [
+      {
+        id: 1,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:30 PM",
+        type: "reciever",
+      },
+      {
+        id: 2,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:40 PM",
+        type: "sender",
+      },
+      {
+        id: 3,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice 😍",
+        timeStamp: "4:50 PM",
+        type: "reciever",
+      },
+      {
+        id: 4,
+        content:
+          " i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:00 PM",
+        type: "reciever",
+      },
+      {
+        id: 5,
+        content: "Random Msg to Test my App",
+        timeStamp: "7:50 PM",
+        type: "sender",
+      },
+    ],
+  },
+  {
+    id: 3,
+    imageUrl: user1,
+    contactName: "Desian Tam",
+    isPinned: true,
+    messages: [
+      {
+        id: 1,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:30 PM",
+        type: "reciever",
+      },
+      {
+        id: 2,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:40 PM",
+        type: "sender",
+      },
+      {
+        id: 3,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice 😍",
+        timeStamp: "4:50 PM",
+        type: "reciever",
+      },
+      {
+        id: 4,
+        content:
+          " i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:00 PM",
+        type: "reciever",
+      },
+      {
+        id: 5,
+        content: "Random Msg to Test my App",
+        timeStamp: "7:50 PM",
+        type: "sender",
+      },
+    ],
+  },
+  {
+    id: 4,
+    imageUrl: user2,
+    contactName: "Killan James",
+    isPinned: true,
+    messages: [
+      {
+        id: 1,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:30 PM",
+        type: "reciever",
+      },
+      {
+        id: 2,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:40 PM",
+        type: "sender",
+      },
+      {
+        id: 3,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice 😍",
+        timeStamp: "4:50 PM",
+        type: "reciever",
+      },
+      {
+        id: 4,
+        content:
+          " i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:00 PM",
+        type: "reciever",
+      },
+      {
+        id: 5,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:10 PM",
+        type: "sender",
+      },
+    ],
+  },
+  {
+    id: 5,
+    imageUrl: user1,
+    contactName: "Desian Tam",
+    isPinned: true,
+    messages: [
+      {
+        id: 1,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:30 PM",
+        type: "reciever",
+      },
+      {
+        id: 2,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:40 PM",
+        type: "sender",
+      },
+      {
+        id: 3,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice 😍",
+        timeStamp: "4:50 PM",
+        type: "reciever",
+      },
+      {
+        id: 4,
+        content:
+          " i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:00 PM",
+        type: "reciever",
+      },
+      {
+        id: 5,
+        content: "Random Msg to Test my App",
+        timeStamp: "7:50 PM",
+        type: "sender",
+      },
+    ],
+  },
+  {
+    id: 6,
+    imageUrl: user2,
+    contactName: "Desian Tam",
+    isPinned: true,
+    messages: [
+      {
+        id: 1,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:30 PM",
+        type: "reciever",
+      },
+      {
+        id: 2,
+        content:
+          "yea I’m well, Thank you, i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "4:40 PM",
+        type: "sender",
+      },
+      {
+        id: 3,
+        content:
+          "Hi, I hope you are doing well, yesterday you have gave a pen This very nice 😍",
+        timeStamp: "4:50 PM",
+        type: "reciever",
+      },
+      {
+        id: 4,
+        content:
+          " i am very happy for this.yesterday you have gave a pen This very nice",
+        timeStamp: "5:00 PM",
+        type: "reciever",
+      },
+      {
+        id: 5,
+        content: "Random Msg to Test my App",
+        timeStamp: "7:50 PM",
+        type: "sender",
+      },
+    ],
+  },
+];
+export default chatData;
